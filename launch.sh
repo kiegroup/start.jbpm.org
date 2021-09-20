e6b46c1b8ea3d8cf1221e1a5fc78dba67139cfed
Ricardo Zanini <zanini@redhat.com>
2021-09-20 16:41:10 -0300

    Improving shell script for CI

--- launch.sh
+++ launch.sh
@@ -20,7 +20,7 @@ if [ "$MVN_ARG_LINE" != "" ] ; then
        mvnBin="$M3_HOME/bin/mvn"
     fi
     echo
-    echo "Running maven build on available projects (using Maven binary '$mvnBin')"
+    echo "----> Running maven build on available projects (using Maven binary '$mvnBin')"
 
     "$mvnBin" -v
     echo
@@ -37,7 +37,7 @@ if [ "$MVN_ARG_LINE" != "" ] ; then
 
                 cd $repository
 
-                "$mvnBin" "${MVN_ARG_LINE[@]}"
+                "$mvnBin" -B "${MVN_ARG_LINE[@]}"
                 returnCode=$?
 
                 if [ $returnCode != 0 ] ; then
@@ -52,23 +52,45 @@ if [ "$MVN_ARG_LINE" != "" ] ; then
     endDateTime=`date +%s`
     spentSeconds=`expr $endDateTime - $startDateTime`
     echo
-    echo "Total build time: ${spentSeconds}s"
+    echo "----> Total build time: ${spentSeconds}s"
 
 else
-    echo "No Maven arguments skipping maven build"
+    echo "----> No Maven arguments skipping maven build"
 fi
 
 if [[ "$@" =~ "docker" ]]; then
-    echo "Launching the application as docker container..."  
+    echo "----> Launching the application as docker container..."  
     docker run -d -p 8090:8090 --name jbpm-bootstrap jbpm/jbpm-bootstrap-service:1.0.0
 else
 
-	echo "Launching the application locally..."
+	echo "----> Launching the application locally..."
 	folder="*.org"
 	cd $folder
 	pattern="*-service"
 	files=( $pattern )
 	cd ${files[0]}
 	executable="$(ls  *target/jbpm*.jar | tail -n1)"
-	java -jar "$executable"
-fi
+
+    ## if on CI, we just verify the 8090 port and close the process
+    if [[ -z "${CI}" ]]; then
+        java -jar "$executable"
+    else
+        java -jar "$executable" &
+
+        APP_PID=$!
+        echo "----> Process PID is ${APP_PID}. Waiting 1m to boot."
+        sleep 1m
+
+        echo "----> Verifying if application is up."
+        status_code=$(curl --write-out '%{http_code}' --silent --output /dev/null http://localhost:8090)
+        exit_status=0
+        if [[ ${status_code} == 200 ]]; then
+            echo "----> Everything looks fine, returning"
+        else   
+            echo "----> Application is not responding, returning."
+            exit_status=1
+        fi
+        kill $APP_PID
+        exit ${exit_status}
+    fi
+fi  
